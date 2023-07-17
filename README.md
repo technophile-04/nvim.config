8a1c59e914d44f3b41f8887e1a0ee7a278e655be
Shiv Bhonde <shivbhonde04@gmail.com>
2023-07-17 10:19:07 +0200

    setup TS and tailwind lsp with some keymaps

---
+++ README.md
@@ -0,0 +1,5 @@
+## 👾 My NeoVim Config
+
+### Screenshot
+
+![Screenshot](./assets/nvim-ss.jpg)
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
+
+-- Perusing code faster with K and J
+vim.keymap.set({ "n", "v" }, "J", "5j", { noremap = true, desc = "Down faster" })
+
+-- Remap K and J
+vim.keymap.set({ "n", "v" }, "<leader>k", "K", { noremap = true, desc = "Keyword" })
+vim.keymap.set({ "n", "v" }, "<leader>j", "J", { noremap = true, desc = "Join lines" })
+
+-- C-P classic
+vim.keymap.set("n", "<C-P>", "<leader>ff")
+
+-- Save file
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { noremap = true, desc = "Save window" })
