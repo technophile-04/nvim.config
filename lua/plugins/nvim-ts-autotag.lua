e0390ba0118dbbbd78df8da8a5bd6b710f9726bf
Shiv Bhonde <shivbhonde04@gmail.com>
2023-08-01 20:15:38 +0530

    setup ts, tw, markdown and emmet

---
+++ lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "astro",
+      "typescript",
+      "html",
+      "javascript",
+      "javascriptreact",
+      "typescriptreact",
+      "tsx",
+      "jsx",
+      "markdown",
+    },
+    config = true,
+  },
+}
