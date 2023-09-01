a9e0294e40702188da7b941f64f91b638d6a6e3a
Shiv Bhonde <shivbhonde04@gmail.com>
2023-09-02 00:17:51 +0530

    fix telescope from switching cwd

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,9 @@
-local Util = require("lazyvim.util")
-
 return {
   {
     "nvim-telescope/telescope.nvim",
     keys = {
-      { "<leader><space>", Util.telescope("files"), desc = "Find Files (root dir)" },
       -- find
       { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-      { "<leader>fF", Util.telescope("files"), desc = "Find Files (root dir)" },
-      { "<leader>ff", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
-      { "<leader>fR", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-      { "<leader>fr", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)" },
       -- add a keymap to browse plugin files
       {
         "<leader>fp",
