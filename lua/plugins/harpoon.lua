52961608a5fe8d1bb7f619c090a1e72a3b0d9c95
Shiv Bhonde <shivbhonde04@gmail.com>
2024-01-07 15:59:35 +0530

    fix harpoon lossing context and hijack_netrw

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -43,5 +43,14 @@ return {
         desc = "Nav file 3",
       },
     },
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup({
+        settings = {
+          save_on_toggle = true,
+          sync_on_ui_close = true,
+        },
+      })
+    end,
   },
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,3 +1,4 @@
+local Util = require("lazyvim.util")
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -6,6 +7,27 @@ return {
       window = {
         position = "float",
       },
+      filesystem = {
+        hijack_netrw_behavior = "open_current",
+      },
+    },
+    keys = {
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = Util.root() })
+        end,
+        desc = "Explorer NeoTree (root dir)",
+      },
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (root dir)", remap = true },
+      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
     },
   },
 }
