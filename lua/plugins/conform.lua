0a480a5d2e400673a97ce1f0d967c3141745fcde
Shiv Bhonde <shivbhonde04@gmail.com>
2024-04-27 19:37:58 +0530

    configure conform and mason

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,36 @@
+local prettierOrBiome = { "biome" }
+return {
+  -- Use Biome instead of prettier / eslint
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["javascript"] = prettierOrBiome,
+        ["javascriptreact"] = prettierOrBiome,
+        ["typescript"] = prettierOrBiome,
+        ["typescriptreact"] = prettierOrBiome,
+        ["vue"] = prettierOrBiome,
+        ["css"] = prettierOrBiome,
+        ["scss"] = prettierOrBiome,
+        ["less"] = prettierOrBiome,
+        ["html"] = prettierOrBiome,
+        ["json"] = prettierOrBiome,
+        ["jsonc"] = prettierOrBiome,
+        ["yaml"] = prettierOrBiome,
+        ["markdown"] = prettierOrBiome,
+        ["markdown.mdx"] = prettierOrBiome,
+        ["graphql"] = prettierOrBiome,
+        ["handlebars"] = prettierOrBiome,
+      },
+    },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "biome",
+      },
+    },
+  },
+}
